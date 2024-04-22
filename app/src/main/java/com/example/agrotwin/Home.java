cb296f64c65342d9c9f18d11e7274295b0424c24
Adolfopgv <adolfopgv@gmail.com>
2024-04-22 14:56:54 +0200

    Home actualizado y funcional con recycledView

--- app/src/main/java/com/example/agrotwin/Home.java
+++ app/src/main/java/com/example/agrotwin/Home.java
@@ -1,79 +1,83 @@
 package com.example.agrotwin;
 
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import android.annotation.SuppressLint;
-import android.content.Intent;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Home extends AppCompatActivity {
 
-    private DrawerLayout drawerLayout;
+    RecyclerView recyclerView;
+    List<DataClass> dataList;
+    MyAdapter adapter;
+    DataClass androidData;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
-        setSupportActionBar(toolbar);
+        recyclerView = findViewById(R.id.recyclerView);
+        searchView = findViewById(R.id.search);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
-                R.string.close_nav);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchList(newText);
+                return true;
+            }
+        });
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
-        }
-    }
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(Home.this, 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        dataList = new ArrayList<>();
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int itemId = item.getItemId();
+        for (int i = 0; i < 10; i++) {
+            androidData = new DataClass("Camera", R.string.camera, "Java", R.drawable.greenhouse);
+            dataList.add(androidData);
 
-        if (itemId == R.id.nav_home) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-        } else if (itemId == R.id.nav_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-        } else if (itemId == R.id.nav_share) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ShareFragment()).commit();
-        } else if (itemId == R.id.nav_about) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragment()).commit();
-        } else if (itemId == R.id.nav_logout) {
-            Intent it = new Intent(getApplicationContext(), MainActivityLogin.class);
-            startActivity(it);
-            Toast.makeText(this, "Logged out!", Toast.LENGTH_SHORT).show();
-            this.finish();
         }
 
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
+//        androidData = new DataClass("RecyclerView", R.string.recyclerview, "Kotlin", R.drawable.greenhouse);
+//        dataList.add(androidData);
+//
+//        androidData = new DataClass("Date Picker", R.string.date, "Java", R.drawable.greenhouse);
+//        dataList.add(androidData);
+//
+//        androidData = new DataClass("EditText", R.string.edit, "Kotlin", R.drawable.greenhouse);
+//        dataList.add(androidData);
+//
+//        androidData = new DataClass("Rating Bar", R.string.rating, "Java", R.drawable.greenhouse);
+//        dataList.add(androidData);
+
+        adapter = new MyAdapter(Home.this, dataList);
+        recyclerView.setAdapter(adapter);
     }
 
-
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
+    private void searchList(String text){
+        List<DataClass> dataSearchList = new ArrayList<>();
+        for (DataClass data : dataList){
+            if (data.getDataTitle().toLowerCase().contains(text.toLowerCase())) {
+                dataSearchList.add(data);
+            }
+        }
+        if (dataSearchList.isEmpty()){
+            Toast.makeText(this, "Not Found", Toast.LENGTH_SHORT).show();
         } else {
-            super.onBackPressed();
+            adapter.setSearchList(dataSearchList);
         }
     }
 }
