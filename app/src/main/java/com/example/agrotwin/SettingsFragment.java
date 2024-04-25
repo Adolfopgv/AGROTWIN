ad85b9be297bfc7b59f21157ba9822f59151c5da
David Pimentel <daviidpiimentel@gmail.com>
2024-04-25 11:34:41 +0200

    Ajustes actualizado | Modo oscuro con Switch y layout

--- app/src/main/java/com/example/agrotwin/SettingsFragment.java
+++ app/src/main/java/com/example/agrotwin/SettingsFragment.java
@@ -1,12 +1,24 @@
 package com.example.agrotwin;
 
+import android.app.PendingIntent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +35,13 @@ public class SettingsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Spinner spinner;
+    public static final String[] languages = {"Select language", "English", "Español"};
+    private Button btnSN;
+    private static final String CHANNEL_ID = "canal";
+    private PendingIntent pendingIntent;
+    boolean isNightModeOn;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -61,8 +80,96 @@ public class SettingsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
-
+        darkMode(view);
+        spinnerMode(view);
+//        notifications();
 
         return view;
+    }
+
+    //    private void notifications() {
+//
+//        btnSN = findViewById(R.id.btnSN);
+//        btnSN.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+//                    //Es true, signifixa que el dispositivo que ejecuta la aplicacion tiene
+//                   // Android SDK, superior - y se ejecutará el bloque de código dentro de usted "if".
+//                    showNotification();
+//                }else{
+//
+//                    showNewNotification();
+//
+//                }
+//            }
+//        });
+
+//    }
+
+//    private void showNotification(){
+//        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "NEW", NotificationManager.IMPORTANCE_DEFAULT);
+//        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//        manager.createNotificationChannel(channel);
+//        showNewNotification();
+//
+//    }
+
+    private void showNewNotification(){
+
+    }
+
+    private void spinnerMode(View view) {
+        spinner = view.findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, languages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setSelection(0);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedLang = parent.getItemAtPosition(position).toString();
+
+                if (selectedLang.equals("English")) {
+                    setLocale("en");
+                } else if (selectedLang.equals("Español")) {
+                    setLocale("es");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+    }
+
+    private void darkMode(View view) {
+        Switch switchDarkMode = view.findViewById(R.id.btnDarkMode);
+
+        switchDarkMode.setChecked(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES);
+
+        switchDarkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+    }
+
+
+    private void setLocale(String langCode) {
+        Locale locale = new Locale(langCode);
+        Locale.setDefault(locale);
+        Resources resources = getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
     }
 }
